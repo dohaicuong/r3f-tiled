ea2bc29f5633fc5f630e35989245ca50d9ce564b
Eric Do <beatyshot@gmail.com>
2022-05-12 19:09:39 +1000

    fix types

--- src/apis/constants.ts
+++ src/apis/constants.ts
@@ -1,4 +1,4 @@
-export const ENV = 'dev'
-export const API_URL = 'https://api.dev.go1.cloud'
-export const PORTAL = 'onboardingadventureextravaganza.dev.go1.cloud'
-export const PORTAL_ID = 12019873
+export const ENV = process.env.NODE_ENV || 'dev'
+export const API_URL = process.env.API_URL || 'https://api.dev.go1.cloud'
+export const PORTAL = process.env.PORTAL || 'onboardingadventureextravaganza.dev.go1.cloud'
+export const PORTAL_ID = process.env.PORTAL_ID || 12019873
